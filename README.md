17dd21b9169775ecfe7ad4538a835d1db7445baf
KimSia, Sim <245021+simkimsia@users.noreply.github.com>
2023-12-17 18:07:49 +0800

    feat: make local OAuth protected endpoint
    with Postman as client
    
    Signed-off-by: KimSia, Sim <245021+simkimsia@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,207 @@
+# OAuth Protected Endpoints for your custom GPT in OpenAI's GPT Store
+
+| Attribute | Details         |
+|-----------|-----------------|
+| Author    | Kimsia          |
+| Date      | 2023-12-10 Sunday |
+| Version   | 0.0.1           |
+
+
+
+## Table of Contents
+
+- **[Why This?](#why-this)**
+- **[Roadmap](#roadmap)**
+  - **[Committed Plans](#committed-plans)**
+  - **[Not Committed Plans](#not-committed-plans)**
+- **[Footnotes](#footnotes)**
+
+
+## Why This?
+
+I created this GitHub repository to show how to implement the hello-world, canonical example of
+implementing OAuth protected endpoints for custom GPTs in OpenAI's GPT Store.
+
+This repository uses micro Django and DigitalOcean App Platform to implement.
+
+For more details about the larger mission, check out the parent repository[^1] for this.
+
+## Who is This For?
+
+This is for people who meet all the requirements:
+
+- You're already building custom GPT in OpenAI
+- You're comfortable with Python code
+- You're comfortable with experimenting with DigitalOcean
+- You're comfortable with googling (or asking ChatGPT) how to setup Python virtual environment to
+  test locally
+- You can figure out how to install Postman on your machine.
+
+## Stages of Progression
+
+I will onboard you in stages. You cannot skip stages else it's very hard to troubleshoot.
+
+1. You setup a OAuth protected endpoint locally using this repository and Postman as the client.
+2. Same as Stage 1 except the OAuth protected endpoint is now on DigitalOcean.
+3. Same as Stage 2 except you're using a test GPT as the client.
+
+
+The objective of this repo is to get you to:
+
+- have OAuth protected endpoints on DigitalOcean App Platform,
+- have your test GPT call your endpoints correctly, and
+- learn how to do basic troubleshooting
+
+
+### Stage 1: Local OAuth protected endpoint with Postman as client
+
+I'm sorry that I'm macOS-first. In future, I will make it convenient for Windows-based developers.
+
+Linux developers, you're on your own. 😊
+
+#### Stage 1 Steps
+
+1. Clone this repo.
+
+Because then it's easier to know where you last stopped.
+
+1. Go to settings and make it private.
+
+Because you will need to add secrets. Check out the footnote[^2] if you don't know how to set to
+private.
+
+3. Make a separate branch from `main` and call it `develop`.
+
+Easier to experiment if you keep your experiment on separate branch.
+
+I expect to update `main` branch in future, so you can setup upstream to pull the latest changes
+from this repo as well.
+
+4. Download and install Postman if you haven't already done so on your machine.
+
+Go to https://www.postman.com/downloads/
+
+5. Setup a virtual environment for this Django installation.
+
+This is a tricky step as different OS will have different sub-steps.
+
+For macOS developers, follow the instructions under `.infrastructure/.macos/README.md`
+
+For windows developers, stay patient. I will have one for you soon.
+
+My early research shows me I have to use https://github.com/pyenv-win/pyenv-win
+
+
+6. Run the Django app locally and test against Postman.
+
+I have compiled the whole thing as a loom video here.
+
+[![Video Title or Description](https://link-to-your-screenshot.jpg)](https://www.loom.com/share/your-video-id)
+
+
+### Stage 2: OAuth protected endpoint on DigitalOcean App Platform with Postman as client
+
+Once you have Stage 1 working, it's time to have your endpoints working on the internet.
+
+#### Stage 2 Steps
+
+1. Register an account with DigitalOcean
+
+If you click on this, you will get $200 credit over the next 60 days.
+
+[![DigitalOcean Referral Badge](https://web-platforms.sfo2.cdn.digitaloceanspaces.com/WWW/Badge%203.svg)](https://www.digitalocean.com/?refcode=1be9f5a28874&utm_campaign=Referral_Invite&utm_medium=Referral_Program&utm_source=badge)
+
+Which is more than enough for an exercise.
+
+2. If you already have an account, you need to setup two things: App, and Database.
+
+Both are meant to be managed services by DigitalOcean.
+
+In this demo, I will choose the absolute cheapest tier. Then, when you're done, you can just shut down. And you will only be charged on pro-rated costs.
+
+I have compiled the whole thing as a loom video here.
+
+[![Video Title or Description](https://link-to-your-screenshot.jpg)](https://www.loom.com/share/your-video-id)
+
+
+### Stage 3: OAuth protected endpoint on DigitalOcean App Platform with custom GPT
+
+1. Create your own GPT
+2. Create your own privacy page using Termly
+3. Add the OAuth protected endpoints to the GPT
+
+I have compiled the whole thing as a loom video here.
+
+[![Video Title or Description](https://link-to-your-screenshot.jpg)](https://www.loom.com/share/your-video-id)
+
+
+## Todo
+
+- [ ] Make Stage 1 Windows-friendly
+
+
+
+I divide my roadmap into [committed plans](#committed-plans) and
+[uncommitted plans](#not-committed-plans).
+
+Both committed and un-committed plans do not come with a committed timeline.
+
+They will be done when they are done. And in no particular order.
+
+If you want to get updated, you can create a GitHub issue and tell me your preferred method of
+getting updates. I am leaning towards a Substack for such purposes. Majority wins.
+
+### Committed Plans
+
+- [ ] A copy-and-paste, hello-world, canonical example[^2] using micro Django and DigitalOcean App
+Platform to set up OAuth-protected endpoints for custom GPT.
+
+- [ ] A Loom video tutorial on how to use micro Django and DigitalOcean App Platform to set up
+OAuth-protected endpoints for custom GPT.
+
+- [ ] A more polished and edited YouTube video tutorial on using micro Django and DigitalOcean App
+Platform.
+
+- [ ] A technology-agnostic guide outlining abstract principles, enabling developers to use other
+technologies (like Laravel, TypeScript, etc.) and infrastructure providers (AWS, Heroku, etc.).
+
+- [ ] A subscription option (likely a Substack) for updates on any OpenAI changes impacting the
+implementation of OAuth-protected endpoints.
+
+### Not Committed Plans
+
+- A directory listing community projects that implement the canonical example using different
+technologies and infrastructures.
+
+## FAQs
+
+1. Is this production-ready?
+
+No, this is meant to demonstrate how to setup OAuth protected endpoints using actual cloud hosting.
+
+But, it's not too hard to make a few changes to make this production ready.
+
+Changes such as changing environmental variables like DEBUG from True to False, upgrading the DigitalOcean App Platform hosting resources, etc.
+
+2. Any unit tests?
+
+No. It's not on the roadmap as well.
+
+But, if it's important to you, open an issue. I might bump it higher on my roadmap.
+
+3. Why is your Django app just a single file?
+
+I am most familiar with Django but its strength (being comprehensive) is also its weakness (looks 'heavy').
+
+I found this O'Reilly chapter called The World's Smallest Django Project[^3]. It basically turns Django into a single python file.
+
+I extended that for use here since this is meant to demonstrate a concept.
+
+I call this way of making Django as lightweight as possible -- micro Django. I have a soft spot for rhymes. 😊
+
+
+[^1]: [GreenDeploy-io/oauth-gpt: Resources and howto for making oauth for your custom gpt in OpenAI's GPT Builder (github.com)](https://github.com/GreenDeploy-io/oauth-gpt-django-doap) last commit on
+2023-12-10 Sunday
+[^2]: [Setting repository visibility - GitHub Docs](https://docs.github.com/en/repositories/managing-your-repositorys-settings-and-features/managing-repository-settings/setting-repository-visibility) last accessed on 2023-12-10 Sunday
+[^3]: [1. The World’s Smallest Django Project - Lightweight Django [Book] (oreilly.com)](https://www.oreilly.com/library/view/lightweight-django/9781491946275/ch01.html) last accessed on 2023-12-10 Sunday
+
---
+++ manage.py
@@ -0,0 +1,252 @@
+import os
+import sys
+
+from decouple import config
+from django.conf import settings
+from django.core.wsgi import get_wsgi_application
+from django.http import JsonResponse  # Updated to use JsonResponse
+from django.shortcuts import redirect, render
+from django.urls import include, path
+from django.views.decorators.http import require_http_methods
+
+BASE_DIR = os.path.dirname(__file__)
+
+# Environment Variables
+DEBUG = config("DEBUG", default=True, cast=bool)
+SECRET_KEY = config("SECRET_KEY", default="your_default_secret_key")
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost").split(",")
+
+STATIC_URL = "/static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+# Django Settings
+settings.configure(
+    DEBUG=DEBUG,
+    SECRET_KEY=SECRET_KEY,
+    ALLOWED_HOSTS=ALLOWED_HOSTS,
+    STATIC_URL="/static/",
+    STATIC_ROOT=os.path.join(BASE_DIR, "staticfiles"),
+    DATABASES={
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": config("DB_NAME", default="your_default_db_name"),
+            "USER": config("DB_USER", default="your_default_db_user"),
+            "PASSWORD": config("DB_PASSWORD", default="your_default_db_password"),
+            "HOST": config("DB_HOST", default="localhost"),
+            "PORT": config("DB_PORT", default="5432"),
+        }
+    },
+    INSTALLED_APPS=[
+        # compulsory
+        "django.contrib.auth",  # For user authentication
+        "django.contrib.contenttypes",  # For content types used by the authentication system
+        "django.contrib.sessions",  # For session management, required if using session-based authentication
+        "django.contrib.staticfiles",  # For serving static files
+        # END compulsory
+        "django.contrib.admin",  # for admin
+        "django.contrib.messages",  # for admin
+        "rest_framework",
+        "oauth2_provider",
+        "corsheaders",  # for oauth
+    ],
+    ROOT_URLCONF=__name__,
+    LOGIN_URL="/admin/login/",  # for auth but using admin
+    MIDDLEWARE=[
+        "corsheaders.middleware.CorsMiddleware",  # for oauth and before any middleware
+        "django.middleware.common.CommonMiddleware",
+        "django.middleware.csrf.CsrfViewMiddleware",
+        "django.middleware.clickjacking.XFrameOptionsMiddleware",
+        "django.contrib.sessions.middleware.SessionMiddleware",  # Required for admin
+        "django.contrib.auth.middleware.AuthenticationMiddleware",  # Required for admin
+        "django.contrib.messages.middleware.MessageMiddleware",  # Required for admin
+    ],
+    # Templates
+    TEMPLATES=[
+        {
+            "BACKEND": "django.template.backends.django.DjangoTemplates",
+            "DIRS": [os.path.join(BASE_DIR, "templates")],
+            "APP_DIRS": True,  # Add this line to enable template loading from plugins
+            "OPTIONS": {
+                "context_processors": [
+                    # ... other context processors ...
+                    "django.template.context_processors.debug",  # for admin
+                    "django.template.context_processors.request",  # for admin
+                    "django.contrib.auth.context_processors.auth",  # for admin
+                    "django.contrib.messages.context_processors.messages",  # for admin
+                ],
+            },
+        },
+    ],
+    CORS_ORIGIN_ALLOW_ALL=True,  # for oauth
+    OAUTH2_PROVIDER={  # for oauth"
+        "PKCE_REQUIRED": False,  # for gpt because they don't support PKCE
+    },
+    REST_FRAMEWORK={
+        "DEFAULT_AUTHENTICATION_CLASSES": [
+            "oauth2_provider.contrib.rest_framework.OAuth2Authentication",
+            # ... other authentication classes ...
+        ],
+        # ... other DRF settings ...
+    },
+)
+
+
+# Ensure django fully utilized before models, etc
+import django
+
+django.setup()
+# End
+
+
+# Serializers
+# ----
+
+from rest_framework import serializers
+
+
+class MyDataSerializer(serializers.Serializer):
+    field1 = serializers.CharField(max_length=100)
+    field2 = serializers.IntegerField()
+
+
+# end of serializers
+
+
+# Views
+def hello_world(request):
+    return JsonResponse({"message": "Hello, World!"})  # Updated endpoint function
+
+
+from marshmallow import Schema, fields
+from webargs.djangoparser import use_args
+
+
+class NameSchema(Schema):
+    name = fields.Str(required=True)
+
+
+from django.views.decorators.csrf import csrf_exempt
+
+
+@csrf_exempt
+@require_http_methods(["POST"])
+@use_args(NameSchema(), location="json")
+def say_my_name(request, request_args):
+    name = request_args.get("name")
+    return JsonResponse({"message": f"Hello, {name}"})
+
+
+def privacy(request):
+    return render(request, "privacy.html")
+
+
+# DRF custom schema
+# from rest_framework.schemas.openapi import AutoSchema
+
+
+# class CustomSchema(AutoSchema):
+#     def get_components(self, path, method):
+#         components = super().get_components(path, method)
+#         # Add your custom components here
+#         components["MyData"] = {
+#             "type": "object",
+#             "properties": {"field1": {"type": "string"}, "field2": {"type": "integer"}},
+#         }
+#         return components
+
+
+# DRF view
+
+# from rest_framework.response import Response
+# from rest_framework.views import APIView
+
+
+# class MyDataView(APIView):
+#     schema = CustomSchema()
+
+#     def get(self, request):
+#         """
+#         get:
+#           summary: Example endpoint returning a data structure
+#           description: An endpoint that returns a simple data structure.
+#           responses:
+#             200:
+#               description: A sample data structure
+#               content:
+#                 application/json:
+#                   schema: MyDataSerializer
+#         """
+#         # Example data
+#         data = {"field1": "value1", "field2": 123}
+#         return Response(MyDataSerializer(data).data)
+
+
+from rest_framework.renderers import JSONOpenAPIRenderer, OpenAPIRenderer
+from rest_framework.schemas import get_schema_view
+
+# Define the schema view
+schema_view = get_schema_view(
+    title="Your API",
+    version="1.0.0",
+    description="Your API description",
+    renderer_classes=[JSONOpenAPIRenderer, OpenAPIRenderer],
+)
+
+# End of DRF
+
+# Django oAuth Toolkit
+
+from oauth2_provider.decorators import protected_resource
+from oauth2_provider.views.generic import ProtectedResourceView
+
+
+class ApiEndpoint(ProtectedResourceView):
+    def get(self, request, *args, **kwargs):
+        return JsonResponse(
+            {"message": "Hello, OAuth2! Talking to you from class based ApiEndpoint"}
+        )  # Updated endpoint function
+
+
+@protected_resource()
+def api_endpoint(request):
+    return JsonResponse(
+        {"message": "Hello, OAuth2! Talking to you from function based api_endpoint"}
+    )  # Updated endpoint function
+
+
+# End of Django oAuth Toolkit
+
+# End of Views
+
+
+# URL Patterns
+
+from django.conf.urls.static import static
+from django.contrib import admin
+
+urlpatterns = [
+    path("", lambda request: redirect("/hello/", permanent=True)),
+    path("admin/", admin.site.urls),  # for admin
+    path("hello/", hello_world),  # Updated path
+    path("privacy/", privacy),  # privacy
+    path("say-my-name/", say_my_name),  # say-my-name
+    path("api/mydata/", MyDataView.as_view()),  # DRF
+    path("openapi/", schema_view, name="openapi-schema"),
+    # path("accounts/", include("django.contrib.auth.urls")),  # for auth
+    path("o/", include("oauth2_provider.urls", namespace="oauth2_provider")),
+    path(
+        "api/class_based/hello", ApiEndpoint.as_view()
+    ),  # an example resource endpoint using class based
+    path(
+        "api/function_based/hello", api_endpoint
+    ),  # an example resource endpoint using function based
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+application = get_wsgi_application()
+
+if __name__ == "__main__":
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
